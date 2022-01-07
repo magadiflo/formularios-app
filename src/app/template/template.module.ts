c866cfbb5cfaf9f72d03f009e0b0fcbab2f1e126
Martín <magadiflo@gmail.com>
2022-01-07 10:16:51 -0500

    Componentes y LazyLoad de formularios

--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -3,9 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { TemplateRoutingModule } from './template-routing.module';
 
+import { BasicosComponent } from './basicos/basicos.component';
+import { DinamicosComponent } from './dinamicos/dinamicos.component';
+import { SwitchesComponent } from './switches/switches.component';
+
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BasicosComponent,
+    DinamicosComponent,
+    SwitchesComponent
+  ],
   imports: [
     CommonModule,
     TemplateRoutingModule
