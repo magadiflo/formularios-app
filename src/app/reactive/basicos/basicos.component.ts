87fe14d25106a2c7e58c4ba36498423bcf8ba2b8
Martín <magadiflo@gmail.com>
2022-01-10 13:00:34 -0500

    Validaciones básicas - Forms Validator

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -9,22 +9,10 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class BasicosComponent {
 
-  /**Para un nuevo campo declaramos en new FormControl..
-  **/
-  // miFormulario: FormGroup = new FormGroup({
-  //   nombre: new FormControl('Procesador Intel'),
-  //   precio: new FormControl(1500),
-  //   existencias: new FormControl(5),
-  // });
-
-  /**
-   * Para evitar estar declarando por cada nuevo campo
-   * un new FormControl, es que usamos mejor el FormBuilder
-   */
   miFormulario: FormGroup = this.fb.group({
-    nombre: ['Procesador Intel'],
-    precio: [0],
-    existencias: [0],
+    nombre: ['Procesador Intel', [Validators.required, Validators.minLength(3)],],
+    precio: [0, [Validators.required, Validators.min(0)]],
+    existencias: [0, [Validators.required, Validators.min(0)]],
   });
 
   constructor(private fb: FormBuilder) { }
