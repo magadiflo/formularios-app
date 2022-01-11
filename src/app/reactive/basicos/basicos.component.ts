81711e7e56b4d1f1e861f8db26ed423ee9ec2bbc
Martín <magadiflo@gmail.com>
2022-01-11 09:59:41 -0500

    Mostrar mensajes de error

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -10,11 +10,15 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class BasicosComponent {
 
   miFormulario: FormGroup = this.fb.group({
-    nombre: ['Procesador Intel', [Validators.required, Validators.minLength(3)],],
-    precio: [0, [Validators.required, Validators.min(0)]],
-    existencias: [0, [Validators.required, Validators.min(0)]],
+    nombre: ['', [Validators.required, Validators.minLength(3)],],
+    precio: [, [Validators.required, Validators.min(0)]],
+    existencias: [, [Validators.required, Validators.min(0)]],
   });
 
   constructor(private fb: FormBuilder) { }
+
+  campoEsValido(campo: string) {
+    return this.miFormulario.controls[campo].errors && this.miFormulario.controls[campo].touched;
+  }
 
 }
