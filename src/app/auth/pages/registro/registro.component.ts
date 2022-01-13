861bdbe7d1bd41435501fc9c49d19a5b854cf04f
Martín <magadiflo@gmail.com>
2022-01-13 12:09:33 -0500

    Mensaje de error personalizado

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -4,7 +4,10 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ValidatorService } from '../../../shared/validator/validator.service';
 import { EmailValidatorService } from '../../../shared/validator/email-validator.service';
 
-
+interface CampoError {
+  validador: string,
+  mensaje: string,
+}
 
 @Component({
   selector: 'app-registro',
@@ -18,11 +21,25 @@ export class RegistroComponent implements OnInit {
     nombre: ['', [Validators.required, Validators.pattern(this.vs.nombreApellidoPattern)]],
     email: ['', [Validators.required, Validators.pattern(this.vs.emailPattern)], [this.emailValidator]],
     username: ['', [Validators.required, this.vs.noPuedeSerStrider]],
-    password: ['', [Validators.required,Validators.minLength(6)]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
     'password-confirm': ['', [Validators.required]],
-  },{
+  }, {
     validators: [this.vs.camposIguales('password', 'password-confirm')],
   });
+
+  get emailErrorMsg(): string {
+    const errors = this.miFormulario.get('email')?.errors;
+    const key = Object.keys(errors!)[0];
+
+    const mensajesError: CampoError[] = [
+      { validador: 'required', mensaje: 'El email es requerido.' },
+      { validador: 'pattern', mensaje: 'Formato de email no válido.' },
+      { validador: 'emailTomado', mensaje: 'El email ya fue tomado.' },
+    ];
+
+    const mensajeError = mensajesError.find((obj: CampoError) => obj.validador == key);
+    return mensajeError?.mensaje || '';
+  }
 
   constructor(
     private fb: FormBuilder,
@@ -41,18 +58,6 @@ export class RegistroComponent implements OnInit {
 
   campoNoValido(campo: string) {
     return this.miFormulario.get(campo)?.invalid && this.miFormulario.get(campo)?.touched;
-  }
-
-  emailRequired() {
-    return this.miFormulario.get('email')?.errors?.['required'] && this.miFormulario.get('email')?.touched;
-  }
-
-  emailFormato() {
-    return this.miFormulario.get('email')?.errors?.['pattern'] && this.miFormulario.get('email')?.touched;
-  }
-
-  emailTomado() {
-    return this.miFormulario.get('email')?.errors?.['emailTomado'] && this.miFormulario.get('email')?.touched;
   }
 
   submitFormulario(): void {
